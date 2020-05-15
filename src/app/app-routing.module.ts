de386d7382eee01e90909f6a7816e3cb6476d26c
Mabooring <maboo2323@gmail.com>
2020-05-15 11:00:46 +0900

    Angularフロントエンド側作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { ProductModule } from './product/product.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  // { path: 'detail', component: ProductDetailComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), ProductModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
